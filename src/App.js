7d806d2bec7612d5f941f594d114de8eecefdef3
MarcosIraola <iraolamarcos@gmail.com>
2020-10-02 20:08:10 +0200

    cambio head y logo

--- src/App.js
+++ src/App.js
@@ -3,28 +3,18 @@ import styles from './App.module.css'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import DescargarPDF from './components/DescargarPDF/descargar.view';
 import Personal from './components/personal/personal.view';
-import CvPage from './pages/cvPage/cvPage.view';
 import MarcosIraola from './components/personal/assets/MarcosIraola.png';
 import Certificados from './pages/certificates/certificados.view';
-
+import CvPage from './pages/cvPage/cvPage.view';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path ='/'>
           <div className={styles.contenedor}>
             <img src={MarcosIraola} className={styles.imagenPerfil}/>
             <CvPage/>
             <span className={styles.fulltime}>AVAILABLE FULL TIME | PART TIME</span>
             <DescargarPDF/>
           </div>
-        </Route>
-        <Route path='/certificates'>
-          <Certificados/>
-        </Route>
-      </Switch>
-    </Router>
   );
 }
 
