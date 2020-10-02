eb04f7b39cd8eb487f9e5b7dacc460a19c0d9ea1
MarcosIraola <iraolamarcos@gmail.com>
2020-10-02 18:41:09 +0200

    Updates

---
+++ precache-manifest.b2374b8893f935d862720b01a1d2f966.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117ef14e28a4f3d6c2d0453538f19303",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "a3248e34e161f6094347",
+    "url": "/cv/static/css/main.f79296e7.chunk.css"
+  },
+  {
+    "revision": "bb9fa66a37fbfb12ed06",
+    "url": "/cv/static/js/2.10f0f2f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cv/static/js/2.10f0f2f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3248e34e161f6094347",
+    "url": "/cv/static/js/main.b0cfc6bd.chunk.js"
+  },
+  {
+    "revision": "9e0e921495960402e4a4",
+    "url": "/cv/static/js/runtime-main.c17789b0.js"
+  },
+  {
+    "revision": "ced577f7ef9fb79711b409958882f648",
+    "url": "/cv/static/media/MarcosIraola.ced577f7.png"
+  },
+  {
+    "revision": "477b0618a55c3bb16c5b6de887959296",
+    "url": "/cv/static/media/android.477b0618.png"
+  },
+  {
+    "revision": "75e4d6d5a0f6b8f4e4030f9b00511ca7",
+    "url": "/cv/static/media/css.75e4d6d5.png"
+  },
+  {
+    "revision": "1da029b3c1a275f00a75e7bb3ab8d9db",
+    "url": "/cv/static/media/firebase.1da029b3.png"
+  },
+  {
+    "revision": "acf172f26b6761ec94a0d4316bdbf6d4",
+    "url": "/cv/static/media/github.acf172f2.png"
+  },
+  {
+    "revision": "31039a53ab57f00b4eff33fb7bcfa2e8",
+    "url": "/cv/static/media/html.31039a53.png"
+  },
+  {
+    "revision": "74e943d82452f81f6a0bb0b51ac6785d",
+    "url": "/cv/static/media/java.74e943d8.png"
+  },
+  {
+    "revision": "6a4c201cef84c174d0d93923f26b168c",
+    "url": "/cv/static/media/javascript.6a4c201c.png"
+  },
+  {
+    "revision": "efd22267efa1c9589fe5611454dfe821",
+    "url": "/cv/static/media/mysql.efd22267.png"
+  },
+  {
+    "revision": "6793474b36b8348d9f3494324f553bf0",
+    "url": "/cv/static/media/photoshop.6793474b.png"
+  },
+  {
+    "revision": "1befcc4ce3ff6c0fcd919302622ca38c",
+    "url": "/cv/static/media/php.1befcc4c.png"
+  },
+  {
+    "revision": "8376eaf7a3e6112db8205a1bd7d6f94d",
+    "url": "/cv/static/media/premier.8376eaf7.png"
+  },
+  {
+    "revision": "f297dfcd8b3e9215ee862f232d5c9d13",
+    "url": "/cv/static/media/react.f297dfcd.png"
+  }
+]);
